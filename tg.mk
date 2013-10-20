516779362336b2b0460d7d05a8571b72cdb49e22
hedwig34 <hedwig34@gmail.com>
2013-10-20 12:19:32 -0400

    Initial changes for Gummy.

--- tg.mk
+++ tg.mk
@@ -1,16 +1,16 @@
 $(call inherit-product, device/samsung/d2usc/full_d2usc.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/Gummy/config/cdma.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2usc TARGET_DEVICE=d2usc BUILD_FINGERPRINT="samsung/d2usc/d2usc:4.1.1/JRO03L/R530UVXALK5:user/release-keys" PRIVATE_BUILD_DESC="d2usc-user 4.1.1 JRO03L R530UVXALK5 release-keys"
 
-PRODUCT_NAME := cm_d2usc
+PRODUCT_NAME := tg_d2usc
 PRODUCT_DEVICE := d2usc
 
