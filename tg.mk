cb9737ea6371f7e9ab7fcd2ab3c9a543039e7557
hedwig34 <hedwig34@gmail.com>
2013-12-02 09:02:23 -0500

    d2usc: Updates from upstream. Squashed commit of the following:
    commit e91a119709346836a8f2fa1f5422d3011cbadf67
    Merge: 5167793 2f30ba3
    Author: hedwig34 <hedwig34@gmail.com>
    Date:   Sun Dec 1 18:02:20 2013 -0500
    
        Merge remote-tracking branch 'cm/cm-11.0' into kk4.4
    
        Conflicts:
        	tg.mk
    
    commit 2f30ba3114eb25438dd24431da20e877751ad9ed
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Mon Oct 28 21:41:02 2013 -0500
    
        d2usc: remove cdma.mk
    
        Change-Id: Ic355a4093a43ab5bc5ccaa7fcc657f0a939725db
    
    Change-Id: I62abed73218dfcd0cc23f7924eacc4b7a006a53d

--- tg.mk
+++ tg.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/d2usc/full_d2usc.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/Gummy/config/cdma.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
